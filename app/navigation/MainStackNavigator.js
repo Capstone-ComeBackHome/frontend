f0e8ac9bc61b4308556f1a4556a43a532f59ec8b
unknown <papago2355@gmail.com>
2022-04-13 16:51:03 +0900

    Test

--- app/navigation/MainStackNavigator.js
+++ app/navigation/MainStackNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+//test
 import HomeTabNavigator from "./HomeTabNavigator";
 
 import ChatScreen from "../screen/Main/ChatScreen";
