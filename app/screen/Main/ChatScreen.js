f842c781122487c0b9ff69a0b3c7f1f6aae90baa
unknown <papago2355@gmail.com>
2022-05-10 22:11:25 +0900

    chat ui done

--- app/screen/Main/ChatScreen.js
+++ app/screen/Main/ChatScreen.js
@@ -1,27 +1,81 @@
-import React, {useState, useEffect} from 'react';
-import {Button, ScrollView} from 'react-native';
+import React, {useState, useEffect, useCallback,} from 'react';
+import {Button, ScrollView, View,StyleSheet} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 
 import AppText from '../../component/AppText';
 import ScreenContainer from '../../component/ScreenContainer';
 import ScreenContainerView from '../../component/ScreenContainerView';
 import NavigationTop from "../../component/NavigationTop";
+import {GiftedChat, Bubble, InputToolbar} from 'react-native-gifted-chat'
+
 
 const ChatScreen = ({navigation}) => {
     const {colors} = useTheme();
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: '언제부터 증상이 시작되었나요?',
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                },
+            },
+        ])
+    }, [])
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    }, [])
+
 
     return (
         <ScreenContainer>
-            <NavigationTop title={"채팅"}/>
-            <ScrollView>
-                <ScreenContainerView>
-                    <AppText>Chat screen</AppText>
-                    <Button title={'go to chat result'} onPress={() => {navigation.navigate('Main', {screen : 'ChatResult'})}}/>
-                </ScreenContainerView>
-            </ScrollView>
+            <NavigationTop title={"AI 진료실"}/>
+            <View style={{paddingTop: 13}}/>
+            <GiftedChat
+                renderAvatar={() => null}
+                renderDay={() => null}
+                renderTime = {() => null}
+                alignTop={true}
+                messages={messages}
+                renderBubble={renderBubble}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+
         </ScreenContainer>
     );
 }
 
+const renderBubble = props => (
+    <Bubble
+        {...props}
+        wrapperStyle={{
+            left: {
+                backgroundColor: '#53B3EE',
+                borderTopLeftRadius: 5,
+                padding: 7,
+            },
+            right: {
+                backgroundColor: '#F5F5F5',
+                borderTopRightRadius: 5,
+                padding: 7,
+            },
+        }}
+        textStyle={{
+            left: {
+                color: '#fff',
+                fontWeight : '700'
+            },
+            right: {
+                color: '#303030',
+                fontWeight : '700'
+            },
+        }}
+    />
+)
 
 export default ChatScreen;
