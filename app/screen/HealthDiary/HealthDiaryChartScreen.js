c66905d89123131eca93c03dff893b0e9ea91468
devohda <fffvvv6@gmail.com>
2022-05-31 10:36:31 +0900

    feat: bubble chart

--- app/screen/HealthDiary/HealthDiaryChartScreen.js
+++ app/screen/HealthDiary/HealthDiaryChartScreen.js
@@ -20,7 +20,7 @@ import score3 from '../../assets/images/disease/score3.png';
 import score4 from '../../assets/images/disease/score4.png';
 import score5 from '../../assets/images/disease/score5.png';
 import {AuthContext} from "../../context/AuthContextProviders";
-import {VictoryArea, VictoryAxis, VictoryChart, VictoryLegend, VictoryLine, VictoryBar, VictoryTheme} from "victory-native";
+import {VictoryChart, VictoryScatter, VictoryLine, VictoryTheme} from "victory-native";
 
 const HealthDiaryChartScreen = ({navigation}) => {
     const {colors} = useTheme();
@@ -42,13 +42,6 @@ const HealthDiaryChartScreen = ({navigation}) => {
     })
     const windowWidth = Dimensions.get('window').width;
     const windowHeight = Dimensions.get('window').height;
-
-    const data = [
-        { quarter: 1, earnings: 13000 },
-        { quarter: 2, earnings: 16500 },
-        { quarter: 3, earnings: 14250 },
-        { quarter: 4, earnings: 19000 }
-    ];
 
     useEffect(() => {
         // 꺾은선 그래프(3달치)
@@ -75,21 +68,28 @@ const HealthDiaryChartScreen = ({navigation}) => {
             }
         }).catch(err => console.error(err))
 
-        // fetch(`http://ec2-3-37-4-131.ap-northeast-2.compute.amazonaws.com:8080/api/v1/calendars/statistics/bubble`, {
-        //     headers: {Authorization: `Bearer ${state.userToken.accessToken}`}
-        // }).then(response => response.json()).then((res) => {
-        //     if (res.result === 'SUCCESS') {
-        //         const {bubbleResponseList} = res.data;
-        //         const data = {labels: [], data: []}
-        //         let sum = bubbleResponseList.reduce((prev, curr) => prev + curr.count, 0);
-        //         bubbleResponseList.forEach(({count, diseaseType, painAverage}) => {
-        //             data.labels.push(diseaseType);
-        //             data.data.push(count/sum);
-        //         })
-        //         console.log(data);
-        //         setBubbleData(data);
-        //     }
-        // }).catch(err => console.error(err))
+        fetch(`http://ec2-3-37-4-131.ap-northeast-2.compute.amazonaws.com:8080/api/v1/calendars/statistics/bubble`, {
+            headers: {Authorization: `Bearer ${state.userToken.accessToken}`}
+        }).then(response => response.json()).then((res) => {
+            if (res.result === 'SUCCESS') {
+                const {bubbleResponseList} = res.data;
+                // const data = []
+                // let sum = bubbleResponseList.reduce((prev, curr) => prev + curr.count, 0);
+                // bubbleResponseList.forEach(({count, diseaseType, painAverage}) => {
+                //     data.labels.push(diseaseType);
+                //     data.data.push(count/sum);
+                // })
+                // console.log(data);
+                // setBubbleData(data);
+                console.log(bubbleResponseList);
+                const bubble = bubbleResponseList.map((data, index) => ({
+                    x: data.diseaseType,
+                    y: data.count,
+                    amount: data.painAverage * 100
+                }))
+                setBubbleData(bubble);
+            }
+        }).catch(err => console.error(err))
     }, [])
 
     return (
@@ -99,32 +99,30 @@ const HealthDiaryChartScreen = ({navigation}) => {
                 <ScrollView>
                     <ScreenContainerView style={styles.chartContainer}>
                         <AppText style={styles.title}>최근 한 달 부위 별 증상 정도</AppText>
-                        {/*{*/}
-                        {/*    bubbleData && (<>*/}
-                        {/*            <VictoryChart>*/}
-                        {/*                <VictoryLine*/}
-                        {/*                    samples={25}*/}
-                        {/*                    y={(d) => Math.sin(5 * Math.PI * d.x)}*/}
-                        {/*                />*/}
-                        {/*                <VictoryLine*/}
-                        {/*                    samples={100}*/}
-                        {/*                    style={{data: {stroke: "red"}}}*/}
-                        {/*                    y={(d) => Math.cos(5 * Math.PI * d.x)}*/}
-                        {/*                />*/}
-                        {/*            </VictoryChart>*/}
-                        {/*        </>*/}
-                        {/*    )*/}
-                        {/*}*/}
+                        {
+                            bubbleData && (<>
+                                    <VictoryChart width={windowWidth}>
+                                        <VictoryScatter data={bubbleData}
+                                                        style={{data: {fill: colors.blue[2]}}}
+                                                        theme={VictoryTheme.material}
+                                                        domain={{y: [0, 5]}}
+                                                        bubbleProperty="amount"
+                                                        maxBubbleSize={30}
+                                                        minBubbleSize={5}/>
+                                    </VictoryChart>
+                                </>
+                            )
+                        }
                     </ScreenContainerView>
                     <ScreenDivideLineLight/>
                     <ScreenContainerView style={styles.chartContainer}>
                         <AppText style={styles.title}>최근 세 달 간 가장 높은 빈도 질병의 증상 추이 </AppText>
                         {
                             lineData &&
-                            <VictoryChart width={350} theme={VictoryTheme.material} domain={{ y: [0, 5] }}>
-                                <VictoryLine data={lineData[0]} x="date" y="score" />
-                                <VictoryLine data={lineData[1]} x="date" y="score" />
-                                <VictoryLine data={lineData[2]} x="date" y="score" />
+                            <VictoryChart width={windowWidth} theme={VictoryTheme.material} domain={{y: [0, 5]}}>
+                                <VictoryLine data={lineData[0]} x="date" y="score"/>
+                                <VictoryLine data={lineData[1]} x="date" y="score"/>
+                                <VictoryLine data={lineData[2]} x="date" y="score"/>
                             </VictoryChart>
                         }
                     </ScreenContainerView>
